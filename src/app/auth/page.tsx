f5dd2b5c8feb11bb7d025706ee100a98873d6f80
itrostik <RosASergeev@stud.kpfu.ru>
2024-01-12 16:12:12 +0300

    делаем грязюку

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -6,52 +6,137 @@ import {
   signOut,
   onAuthStateChanged,
   GoogleAuthProvider,
-  User,
+  GithubAuthProvider,
+  isSignInWithEmailLink,
+  signInWithEmailLink,
 } from "firebase/auth";
+
+import Form from "@/components/Form";
 
 import { auth } from "../../../firebase/firebase";
 import Image from "next/image";
 import { userType } from "../../../types/userType";
+import { useRouter } from "next/navigation";
+import { addUser, findUser } from "../../../utils/firestore";
+import { loginEmail, loginGithub, loginGoogle } from "../../../utils/auth";
 
 export default function Page() {
   const [user, setUser] = useState<userType | null>(null);
 
-  async function login() {
-    const provider = new GoogleAuthProvider();
-    const result = await signInWithPopup(auth, provider);
-    setUser({
-      photoURL: result.user.photoURL,
-      displayName: result.user.displayName,
-    });
-  }
+  const router = useRouter();
 
-  async function logout() {
-    await signOut(auth);
+  async function login(typeAuth: string) {
+    let user = null;
+    if (typeAuth === "google") user = await loginGoogle();
+    else if (typeAuth === "github") user = await loginGithub();
+
+    // const provider = new GoogleAuthProvider();
+    // const result = await signInWithPopup(auth, provider);
+    // const token = await result.user.getIdToken();
+    // localStorage.setItem("token", token);
+    // setUser({
+    //   picture: result.user.photoURL,
+    //   name: result.user.displayName,
+    //   user_id: result.user.uid,
+    // });
+    // console.log(result);
+    if (user) {
+      setUser({
+        picture: user.photoURL,
+        name: user.displayName,
+        user_id: user.uid,
+        email: user.email,
+      });
+    }
   }
 
   useEffect(() => {
-    const unsub = onAuthStateChanged(auth, (currentUser) => {
-      setUser({
-        photoURL: currentUser?.photoURL,
-        displayName: currentUser?.displayName,
-      });
+    const unsub = onAuthStateChanged(auth, async (currentUser) => {
+      if (currentUser) {
+        const providerId = currentUser.providerData[0].providerId;
+        const providerEmail = currentUser.providerData[0].email;
+        const user: userType = {
+          picture: currentUser.photoURL,
+          name: currentUser.displayName,
+          user_id: currentUser.uid,
+          email: currentUser.email ? currentUser.email : providerEmail,
+        };
+        setUser(user);
+        if (user.email && !(await findUser(user.email, providerId))) {
+          await addUser(user, providerId);
+        } else if (providerEmail) {
+          if (!(await findUser(providerEmail, providerId))) {
+            const bufferUser = {
+              ...user,
+              email: providerEmail,
+            };
+            await addUser(bufferUser, providerId);
+          }
+        }
+      } else {
+        setUser(null);
+      }
     });
     return () => unsub();
-  }, [user]);
+  }, []);
+
+  useEffect(() => {
+    if (user) {
+      router.push("/");
+    }
+  }, [router, user]);
 
   return (
-    <>
-      {!user ? (
-        <div onClick={() => login()}>Войти с помощью гугл</div>
-      ) : (
-        <div>
-          {user.displayName}
-          {user.photoURL && (
-            <Image src={user.photoURL} alt="" width={30} height={30} />
-          )}
-          <div onClick={() => logout()}>Выйти</div>
-        </div>
+    <div
+      className={
+        "bg-black h-screen w-full flex flex-col gap-5 items-center justify-center"
+      }
+    >
+      {!user && (
+        <>
+          <Image
+            src={"/logo.svg"}
+            width={100}
+            height={100}
+            alt={"52"}
+            priority
+          />
+          <span className={"text-white font-medium"}>Авторизация</span>
+
+          <Form setUser={setUser} />
+
+          <span className={"text-white"}>или</span>
+
+          <div className={"flex gap-2"}>
+            <div
+              onClick={() => login("google")}
+              className={
+                "bg-white cursor-pointer flex p-2 gap-2 items-center rounded"
+              }
+            >
+              <Image
+                src={"/google.svg"}
+                width={25}
+                height={25}
+                alt={"google"}
+              />
+            </div>
+            <div
+              onClick={() => login("github")}
+              className={
+                "bg-white cursor-pointer flex p-2 gap-2 items-center rounded"
+              }
+            >
+              <Image
+                src={"/github.svg"}
+                width={30}
+                height={30}
+                alt={"google"}
+              />
+            </div>
+          </div>
+        </>
       )}
-    </>
+    </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,92 @@
+"use client";
+
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+import { jwtDecode, JwtPayload } from "jwt-decode";
+import { userType } from "../../types/userType";
+import { signOut } from "firebase/auth";
+import { auth } from "../../firebase/firebase";
 
 export default function Home() {
+  const [user, setUser] = useState<userType | null>(null);
+
+  const router = useRouter();
+  if (!localStorage.getItem("token")) {
+  }
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      const token = localStorage.getItem("token")!;
+      const decoded = jwtDecode<JwtPayload & userType>(token);
+      setUser({
+        picture: decoded.picture,
+        name: decoded.name,
+        user_id: decoded.user_id,
+        email: decoded.email,
+      });
+    } else {
+      router.push("/auth");
+    }
+  }, [router]);
+
+  async function logout() {
+    try {
+      await signOut(auth);
+      localStorage.removeItem("token");
+      setUser(null);
+      router.push("/auth");
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
-    <main>
-      <span className="w-full flex bg-blue-600">123</span>
-      <span></span>
-      <span></span>
-    </main>
+    <>
+      {user && (
+        <div
+          className={"bg-black w-full h-screen p-3 text-white flex flex-col"}
+        >
+          <header
+            className={
+              "flex justify-between p-2 border-solid border-2 rounded-3xl border-white text-white"
+            }
+          >
+            <div>
+              <Image src={"/logo.svg"} width={40} height={40} alt={"logo"} />
+            </div>
+            <div className={"flex gap-2 items-center justify-end"}>
+              <span>{user.name}</span>
+              {user.picture && (
+                <Image
+                  src={user.picture}
+                  alt=""
+                  width={30}
+                  height={30}
+                  className={"rounded-full"}
+                />
+              )}
+              <div
+                onClick={() => logout()}
+                className={
+                  "mx-3 bg-white text-black p-1 rounded cursor-pointer hover:bg-opacity-70"
+                }
+              >
+                Выйти
+              </div>
+            </div>
+          </header>
+          <main className={"flex-1"}>52</main>
+          <footer
+            className={
+              "flex justify-center items-center font-bold p-2 border-solid border-2 rounded-3xl border-white"
+            }
+          >
+            created by itrostik
+          </footer>
+        </div>
+      )}
+    </>
   );
 }
